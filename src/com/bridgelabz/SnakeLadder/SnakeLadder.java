3544ac1efbd9190f9df5a69fbd482335f1a3b87a
usha-12 <usha12ahirwar@gmail.com>
2022-10-12 21:30:04 +0530

    Welcome to the snake Ladder program

--- src/com/bridgelabz/SnakeLadder/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder/SnakeLadder.java
@@ -1,2 +1,8 @@
-package com.bridgelabz.SnakeLadder;public class SnakeLadder {
+package com.bridgelabz.SnakeLadder;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Snake & Ladder Simulator");
+    }
+
 }
