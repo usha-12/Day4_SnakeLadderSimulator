91e22d737e84934677ba712f6073ddb08653b1e3
usha-12 <usha12ahirwar@gmail.com>
2022-10-12 21:44:59 +0530

    Plyed with single player at start position 0

--- src/com/bridgelabz/SnakeLadder/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder/SnakeLadder.java
@@ -1,8 +1,14 @@
 package com.bridgelabz.SnakeLadder;
 
 public class SnakeLadder {
+    public static final int POSITION = 0;
     public static void main(String[] args) {
         System.out.println("Snake & Ladder Simulator");
+        //Snake and Ladder game played with single player at start position 0
+        System.out.println("Let's begin the game ....");
+        System.out.println("Initial position is Square Zero");
+        System.out.println("position : Square "+POSITION);
+
     }
 
 }
